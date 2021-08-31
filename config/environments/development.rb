a11604321b9fc3512bc71b313f5a6d9064dc6a2c
Jared Thomas <85851208+Jatded1d9l7@users.noreply.github.com>
2021-08-31 17:45:23 -0400

    adding react components

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,6 +11,10 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
+  #devise
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+
   # Show full error reports.
   config.consider_all_requests_local = true
 
@@ -75,4 +79,3 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 end
 
-config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,43 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+users = [
+    {
+      email: 'helloworld@test.com',
+      password: 'helloworld123',
+      password_confirmation: 'helloworld123'
+    }
+  ]
+  
+  users.each do |attribute|
+    User.create attribute
+  end
+  
+  apartments = [
+    {
+      street: '10th Street',
+      city: 'Huntington',
+      state: 'WV',
+      manager: 'john',
+      email: 'john@helloworld.com',
+      price: '500',
+      bedrooms: 1,
+      bathrooms: 1,
+      pets: 'all pets welcome'
+    },
+    {
+      street: '11th Street',
+      city: 'Charleston',
+      state: 'WV',
+      manager: 'Bella',
+      email: 'Bella@helloworld.com',
+      price: '1000',
+      bedrooms: 2,
+      bathrooms: 3,
+      pets: 'No pets allowed'
+    }
+  ]
+  
+  first_user = User.where(email: 'testing@test.com').first
+  
+  apartments.each do |attribute|
+    first_user.apartments.create attribute
+  end
+  
